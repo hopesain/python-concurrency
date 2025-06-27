dc0226bf9de2b97a10d95bb0ae43c06af47e5d4e
hopesain <sainhope16@gmail.com>
2025-06-27 06:49:05 +0200

    just some changes, lol

---
+++ asynchronous_programming/06_running_tasks_concurrently.py
@@ -0,0 +1,64 @@
+### RUNNING TASKS CONCURRENTLY USING asyncio.gather(*aws, return_exceptions=False) ###
+"""
+Run awaitable objects in the aws sequence concurrently.
+
+If any awaitable in aws(awaitables) is a coroutine, it is automatically scheduled as a Task.
+
+If all awaitables are completed successfully, the result is an aggregate list of returned values. The order of result values corresponds to the order of awaitables in aws.
+
+If return_exceptions is False (default), the first raised exception is immediately propagated to the task that awaits on gather(). Other awaitables in the aws sequence won’t be cancelled and will continue to run.
+
+If return_exceptions is True, exceptions are treated the same as successful results, and aggregated in the result list.
+
+If gather() is cancelled, all submitted awaitables (that have not completed yet) are also cancelled.
+
+If any Task or Future from the aws sequence is cancelled, it is treated as if it raised CancelledError – the gather() call is not cancelled in this case. This is to prevent the cancellation of one submitted Task/Future to cause other Tasks/Futures to be cancelled.    
+"""
+
+import asyncio
+from datetime import datetime
+
+
+def get_time(): 
+    return datetime.now().strftime("%H:%M:%S.%f")[:-3]
+
+def time_taken(initial_time, final_time):
+    duration = final_time - initial_time
+    return duration
+
+# TO BETTER UNDERSTAND THE CONCEPT, LET'S SIMULATE A SITUATION WHERE YOU ARE THE CHEF COOKING MULTIPLE DISHES IN THE KITCHEN.
+"""
+You are preparing three dishes.
+Each dish has multiple steps (e.g., prep, cook, plate).
+Each step takes 1 second.
+Instead of cooking them one after another, we’ll use asyncio.gather() to switch between tasks as each step waits.
+"""
+
+async def cook_dish(dish_name, steps):
+    print(f"Starting to prepare {dish_name} at {get_time()}")
+    for i in range(1, steps + 1):
+        print(f"{dish_name} - Step {i} started at {get_time()} - Step {i}/{steps}")
+        await asyncio.sleep(1)
+    print(f"{dish_name} is ready at {get_time()}")
+    return f"{dish_name} is completed!"
+
+async def main():
+    print(f"Kitchen opens up and start to cook at {get_time()}")
+    initial_time = datetime.now()
+    
+    results = await asyncio.gather(
+        cook_dish("Nsima", 4),
+        cook_dish("Soya pieces", 3),
+        cook_dish("Mabilinganya", 2),
+    ) # Notice that we have not specified the return_exceptions parameter, so it defaults to False.
+    
+    print(f"[{get_time()}] 🧾 All dishes done!")
+    print("🍽️ Summary:")
+    for r in results:
+        print(" -", r)
+    
+    final_time = datetime.now()
+    duration = time_taken(initial_time, final_time)
+    print(f"Time taken: {duration} seconds")    
+        
+asyncio.run(main())
---
+++ asynchronous_programming/07_mastering_loops.py
@@ -0,0 +1,108 @@
+#### MASTERING LOOPS IN PYTHON ####
+"""
+The only reason I had to include this section is that things were getting messy as I forgot much information I already covered.
+This is completely normal, and I am sure you will also forget some information as you progress.
+So, it is okay to revisit topics and refresh your memory.
+This section is a summary of loops in Python, including for loops, while loops, range, and iterators.
+"""
+
+### Loops in Python ###
+"""
+Loops allow us to repeat a block of code multiple times until a certain condition is met. 
+In Python, we have two types of loops: for loops and while loops. The type of loop to use depends totally on your problem set. 
+The two types of loops (in python) are described with examples below.
+"""
+
+### For Loops in Python ###
+"""
+For loops are used to iterate over a sequence (like a list, tuple, or string) or any iterable object.
+"""
+
+### Example of a for loop ###
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers: # This is a for loop that iterates over each element in the numbers list.
+    print(number) # This prints each number in the list.
+print("________________________________________")
+
+
+# Another example of using a for loop. 
+"""
+Imagine that we have a list of temperatures in Celsius and want to convert them into Fahrenheit
+"""
+temperatures_celsius = [22, 25, 17, 15, 18]
+temperatures_fahrenheit = []
+
+for temp in temperatures_celsius:
+    temperatures_fahrenheit.append((temp * 9/5) + 32)
+
+print(temperatures_fahrenheit)
+print("________________________________________")
+
+
+### Nested For Loops in Python ###
+"""
+Nested for loops allow us to iterate over multiple sequences or perform operations that require multiple iterations.
+"""
+sensors = ["Temperature", "Humidity", "Pressure"]
+days = ["Monday", "Tuesday", "Wednesday"]
+
+for sensor in sensors:
+    for day in days:
+        print(f"Reading {sensor} on {day}")
+print("________________________________________")
+
+    
+### You can also use the range() function to generate a sequence of numbers. ###
+numbers_range = range(5)
+
+for number in numbers_range:
+    print(number)  # This prints numbers from 0 to 4 (5 is not included).
+print("________________________________________")
+
+
+
+### The range function can also take three arguments: start (optional), stop (required), and step (optional). ###
+# In the above example, we can change the range to start from 1 and go up to 5.
+
+numbers_range = range(1, 6)  # This generates numbers from 1 to 5 (6 is not included).
+
+for number in numbers_range:
+    print(number)  # This prints numbers from 1 to 5.
+print("________________________________________")
+
+
+numbers_collection_range = range(1, 11, 2)  # This generates numbers from 1 to 10 with a step of 2.
+
+for number in numbers_collection_range:
+    print(number)  # This prints odd numbers from 1 to 9.
+print("________________________________________")
+
+
+
+### While Loops in Python ###
+"""
+A while loop executes a block of code as long as a certain condition is True.
+"""
+
+count = 10
+
+while count > 0:
+    print(count)  # This prints the current value of count.
+    count -= 1  # This decreases the value of count by 1 in each iteration
+print("_________________________________________")
+
+
+
+
+
+
+
+fruits = ["apple", "orange", "kiwi"]
+iter_obj = iter(fruits)
+while True:
+    try:
+        fruit = next(iter_obj)
+        print(fruit)
+    except StopIteration:
+        break
